609a19ec635752d057a777ee808641e25141a333
Yuto Oshiro <e195732@ie.u-ryukyu.ac.jp>
2020-01-02 15:43:08 +0900

    実装途中のコード。Pachimon.javaに改善の余地あり

--- src/main/java/jp/ac/uryukyu/ie/e195732/Skill.java
+++ src/main/java/jp/ac/uryukyu/ie/e195732/Skill.java
@@ -1,9 +1,5 @@
 package jp.ac.uryukyu.ie.e195732;
 
-abstract class Skill {
-    protected String name;
-    protected int power;
-    protected String skillType;
+class Skill {
 
-    abstract int changeSkill();
 }
